2b393ee4aff3a0651e657624755ac547b32d1d15
Md Saney Alam <codecamper@outlook.com>
2019-01-19 22:37:52 -0500

    Initial Commit

---
+++ FitnessFrog/Controllers/EntriesController.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using Treehouse.FitnessFrog.Data;
+using Treehouse.FitnessFrog.Models;
+
+namespace Treehouse.FitnessFrog.Controllers
+{
+    public class EntriesController : Controller
+    {
+        private EntriesRepository _entriesRepository = null;
+
+        public EntriesController()
+        {
+            _entriesRepository = new EntriesRepository();
+        }
+
+        public IActionResult Index()
+        {
+            List<Entry> entries = _entriesRepository.GetEntries();
+
+            // Calculate the total activity.
+            double totalActivity = entries
+                .Where(e => e.Exclude == false)
+                .Sum(e => e.Duration);
+
+            // Determine the number of days that have entries.
+            int numberOfActiveDays = entries
+                .Select(e => e.Date)
+                .Distinct()
+                .Count();
+
+            ViewBag.TotalActivity = totalActivity;
+            ViewBag.AverageDailyActivity = (totalActivity / (double)numberOfActiveDays);
+
+            return View(entries);
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(string date, int activityId, string duration, string intensity, string exclude)
+        {
+            return View();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return View();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+
+            return View();
+        }
+    }
+}
--- FitnessFrog/Startup.cs
+++ FitnessFrog/Startup.cs
@@ -44,7 +44,7 @@ namespace FitnessFrog
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
+                app.UseExceptionHandler("Error");
                 app.UseHsts();
             }
 
@@ -56,7 +56,7 @@ namespace FitnessFrog
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Entries}/{action=Index}/{id?}");
             });
         }
     }
